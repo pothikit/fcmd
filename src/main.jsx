be0ee3f59add094596793c3cea27258abb0b9773
pothikit <tytanhsot71@gmail.com>
2024-01-27 14:01:43 +0600

    icon url change json calling from public

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import "./app.css"
 import { RouterProvider } from 'react-router-dom'
 import router from './router/routers'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
