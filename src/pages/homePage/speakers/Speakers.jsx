e21d8e3de47cb8619ec4819e339147cf75bbfeb5
pothikit <tytanhsot71@gmail.com>
2024-01-29 15:59:12 +0600

    team section hover effect with social icons

--- src/pages/homePage/speakers/Speakers.jsx
+++ src/pages/homePage/speakers/Speakers.jsx
@@ -4,6 +4,9 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { MdOutlineArrowBackIos } from "react-icons/md";
 import { MdOutlineArrowForwardIos } from "react-icons/md";
+import { Link } from 'react-router-dom';
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa6';
+import { BsTwitterX } from 'react-icons/bs';
 
 function Speakers() {
     const { data: speaker } = useQuery({
@@ -20,11 +23,19 @@ function Speakers() {
                 <div className='grid grid-cols-2 lg:grid-cols-4 gap-2 sm:gap-7'>
                     {
                         speaker?.map((speaker, idx) => (
-                            <div key={idx} className='p-1 sm:p-2 lg:p-4 shadow-md rounded-md space-y-4 border'>
+                            <div key={idx} className='p-1 sm:p-2 lg:p-4 shadow-md rounded-md space-y-4 border overflow-hidden group cursor-pointer'>
                                 <img src={speaker?.speake_img} alt={speaker?.speakesName} />
-                                <div>
-                                    <h4 className='italic text-sm sm:text-base font-bold md:text-2xl'>{speaker?.speakesName}</h4>
-                                    <h4 className='text-sm md:text-base'>{speaker?.designation}</h4>
+                                <div className='h-12 relative'>
+                                    <div className='absolute left-0 group-hover:left-[-300%] top-0 duration-300'>
+                                        <h4 className='italic text-sm sm:text-base font-bold md:text-2xl'>{speaker?.speakesName}</h4>
+                                        <h4 className='text-sm md:text-base'>{speaker?.designation}</h4>
+                                    </div>
+                                    <div className='absolute left-[300%] group-hover:left-0 top-0 duration-300'>
+                                        <ul className='flex gap-5'>
+                                            <li><Link className='hover:scale-125 duration-200 block'><FaFacebook size={24} /></Link></li>
+                                            <li><Link className='hover:scale-125 duration-200 block'><FaInstagram size={24} /></Link></li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         ))
--- src/shared/footer/Footer.jsx
+++ src/shared/footer/Footer.jsx
@@ -6,6 +6,7 @@ import { BsTwitterX } from "react-icons/bs";
 import footerLogo from "../../assets/footeLogo.png"
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import { FaYoutube } from "react-icons/fa6";
 
 
 // import logo from
@@ -25,17 +26,17 @@ function Footer() {
                         <img src={footerLogo} alt="FCMD" className='max-w-[200px]' />
                         <p className='max-w-[350px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendiss</p>
                         <ul className='flex gap-5'>
-                            <li><Link><FaFacebook size={24} /></Link></li>
-                            <li><Link><FaInstagram size={24} /></Link></li>
-                            <li><Link><BsTwitterX size={24} /></Link></li>
-                            <li><Link><FaLinkedin size={24} /></Link></li>
-                            <li><Link><FaLinkedin size={24} /></Link></li>
+                            <li><Link className='hover:scale-125 duration-200 block'><FaFacebook size={24} /></Link></li>
+                            <li><Link className='hover:scale-125 duration-200 block'><FaInstagram size={24} /></Link></li>
+                            <li><Link className='hover:scale-125 duration-200 block'><BsTwitterX size={24} /></Link></li>
+                            <li><Link className='hover:scale-125 duration-200 block'><FaLinkedin size={24} /></Link></li>
+                            <li><Link className='hover:scale-125 duration-200 block'><FaYoutube size={24} /></Link></li>
                         </ul>
                     </div>
                     <div className='lg:w-7/12 w-full flex justify-between flex-col md:flex-row gap-10'>
                         <div className='flex w-full md:justify-evenly'>
                             <div className='w-1/2 md:w-auto'>
-                                <h2 className='text-sm font-bold mb-5'>Helpful Link</h2>
+                                <h2 className='text-lg font-bold mb-5'>Helpful Link</h2>
                                 <ul className='space-y-3'>
                                     <li><Link>Blood Donation</Link></li>
                                     <li><Link>Admin Panel</Link></li>
@@ -45,7 +46,7 @@ function Footer() {
                                 </ul>
                             </div>
                             <div className='w-1/2 md:w-auto'>
-                                <h2 className='text-sm font-bold mb-5'>Blog</h2>
+                                <h2 className='text-lg font-bold mb-5'>Blog</h2>
                                 <ul className='space-y-3'>
                                     <li><Link>Volunteering</Link></li>
                                     <li><Link>Last Event</Link></li>
@@ -55,18 +56,18 @@ function Footer() {
                             </div>
                         </div>
                         <div className="w-full md:w-auto">
-                            <h2 className='text-sm font-bold mb-5'>Stay up to date</h2>
+                            <h2 className='text-lg font-bold mb-5'>Stay up to date</h2>
                             <form action="" className='flex flex-wrap sm:flex-nowrap gap-2'>
                                 <input type="email" placeholder='Enter your email' className='w-fullw rounded-lg p-2 bg-transparent text-white border outline-none placeholder:text-white' />
                                 <input type="submit" value="Subscribe" className='py-2 px-2 sm:px-3 bg-white text-primary rounded-lg' />
                             </form>
                             <div className='pt-10'>
-                                <h3>Web Contributor</h3>
-                                <p>Asraful Islam Hanif, Mehedi hasan</p>
+                                <h3 className='font-bold'>Web Contributor</h3>
+                                {/* <p>Asraful Islam Hanif, Mehedi hasan</p> */}
                                 <div className='flex items-center gap-3 mt-5 flex-wrap lg:flex-nowrap'>
                                     {
                                         contributors?.slice(0, 4).map((contributor, idx) => (
-                                            <Link key={idx} to={contributor?.website_url} className='p-1 bg-white rounded-md'>
+                                            <Link key={idx} to={contributor?.website_url} className='p-1 bg-white rounded-md h-10 flex items-center hover:scale-110 duration-200'>
                                                 <img src={contributor?.logo} alt={contributor?.sponsorsName} className='max-w-20' />
                                             </Link>
                                         ))
@@ -76,7 +77,7 @@ function Footer() {
                         </div>
                     </div>
                 </div>
-                <div className="footer-bottom border-t-2 py-10 mt-20 flex justify-between flex-col md:flex-row gap-5">
+                <div className="footer-bottom border-t py-10 mt-20 flex justify-between flex-col md:flex-row gap-5">
                     <h3 className='text-white'>&copy; 2023 FCMD. All rights reserved.</h3>
                     <ul className='text-white flex items-center gap-5'>
                         <li><Link>Terms</Link></li>
